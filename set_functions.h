1668fb66afc7868875e11a60f74f62de473342fe
christmas-fire <vpiskin335@gmail.com>
2024-03-30 21:29:33 +0700

    - создал заголовочный файл где я объявляю функции из файла set_functions.cpp

---
+++ set_functions.h
@@ -0,0 +1,29 @@
+#pragma once
+
+    // Библиотеки C++
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+    // Библиотеки C
+#include <stdlib.h>
+#include <time.h>
+
+    // Пространства имен
+using namespace std;
+using namespace sf;
+
+void setWindow(RenderWindow& window, int width, int height);
+
+void setFont(Font& font);
+
+void setIcon(Image& icon, RenderWindow& window);
+
+void setTextures(Texture& texture_apple,
+                        Texture& texture_strawberry,
+                        Texture& texture_snake_head,
+                        Texture& texture_snake_part
+                        );
+
+
